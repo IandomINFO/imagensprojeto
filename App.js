8bea9697f84be61ab73c5f472fcfc009f051c0b1
ian <ianrobertoestudos10@gmail.com>
2023-03-20 14:56:21 -0300

    url state com flat list agora

--- App.js
+++ App.js
@@ -1,22 +1,47 @@
-import { StyleSheet, ScrollView, View } from "react-native";
+import { StyleSheet, ScrollView, View, FlatList } from "react-native";
 import React, { Component } from "react";
 import Imagem from "./src/components/Imagem.js";
 export default class App extends Component {
+  state = {
+    urls: [
+      {
+        livro:
+          "https://www.petz.com.br/blog/wp-content/uploads/2017/04/comportamento-dos-gatos-1.jpg",
+      },
+      {
+        livro:
+          "https://i.pinimg.com/originals/72/73/2d/72732d2a5b02ea432a88ddb0902df1a4.jpg",
+      },
+      {
+        livro:
+          "https://www.chickensmoothie.com/oekaki/image/image.php?id=4114537&size=large&format=auto&rev=1675343459",
+      },
+      {
+        livro:
+          "https://i1.sndcdn.com/artworks-FZYtcnfgLHJp52Q2-lEfruQ-t500x500.jpg",
+      },
+      {
+        livro:
+          "https://i1.sndcdn.com/artworks-FZYtcnfgLHJp52Q2-lEfruQ-t500x500.jpg",
+      },
+      {
+        livro:
+          "https://i1.sndcdn.com/artworks-FZYtcnfgLHJp52Q2-lEfruQ-t500x500.jpg",
+      },
+      {
+        livro:
+          "https://i1.sndcdn.com/artworks-FZYtcnfgLHJp52Q2-lEfruQ-t500x500.jpg",
+      },
+    ],
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <ScrollView>
-          <Imagem imagem="https://www.petz.com.br/blog/wp-content/uploads/2017/04/comportamento-dos-gatos-1.jpg" />
-          <Imagem imagem="https://i.pinimg.com/originals/72/73/2d/72732d2a5b02ea432a88ddb0902df1a4.jpg" />
-          <Imagem imagem="https://www.chickensmoothie.com/oekaki/image/image.php?id=4114537&size=large&format=auto&rev=1675343459" />
-          <Imagem imagem="https://i1.sndcdn.com/artworks-FZYtcnfgLHJp52Q2-lEfruQ-t500x500.jpg" />
-          <Imagem imagem="https://www.chickensmoothie.com/oekaki/image/image.php?id=4114537&size=large&format=auto&rev=1675343459" />
-          <Imagem imagem="https://i1.sndcdn.com/artworks-FZYtcnfgLHJp52Q2-lEfruQ-t500x500.jpg" />
-          <Imagem imagem="https://www.chickensmoothie.com/oekaki/image/image.php?id=4114537&size=large&format=auto&rev=1675343459" />
-          <Imagem imagem="https://i1.sndcdn.com/artworks-FZYtcnfgLHJp52Q2-lEfruQ-t500x500.jpg" />
-          <Imagem imagem="https://www.chickensmoothie.com/oekaki/image/image.php?id=4114537&size=large&format=auto&rev=1675343459" />
-          <Imagem imagem="https://i1.sndcdn.com/artworks-FZYtcnfgLHJp52Q2-lEfruQ-t500x500.jpg" />
-        </ScrollView>
+        <FlatList
+          data={this.state.urls}
+          renderItem={({ item }) => <Imagem imagem={item.livro} />}
+        />
       </View>
     );
   }
