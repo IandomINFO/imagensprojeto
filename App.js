160454b7d01ffa0a28c46d5c60fbd719ea869c62
ian <ianrobertoestudos10@gmail.com>
2023-03-20 15:00:06 -0300

    só mudando o nome pra imagem

--- App.js
+++ App.js
@@ -5,31 +5,31 @@ export default class App extends Component {
   state = {
     urls: [
       {
-        livro:
+        imagem:
           "https://www.petz.com.br/blog/wp-content/uploads/2017/04/comportamento-dos-gatos-1.jpg",
       },
       {
-        livro:
+        imagem:
           "https://i.pinimg.com/originals/72/73/2d/72732d2a5b02ea432a88ddb0902df1a4.jpg",
       },
       {
-        livro:
+        imagem:
           "https://www.chickensmoothie.com/oekaki/image/image.php?id=4114537&size=large&format=auto&rev=1675343459",
       },
       {
-        livro:
+        imagem:
           "https://i1.sndcdn.com/artworks-FZYtcnfgLHJp52Q2-lEfruQ-t500x500.jpg",
       },
       {
-        livro:
+        imagem:
           "https://i1.sndcdn.com/artworks-FZYtcnfgLHJp52Q2-lEfruQ-t500x500.jpg",
       },
       {
-        livro:
+        imagem:
           "https://i1.sndcdn.com/artworks-FZYtcnfgLHJp52Q2-lEfruQ-t500x500.jpg",
       },
       {
-        livro:
+        imagem:
           "https://i1.sndcdn.com/artworks-FZYtcnfgLHJp52Q2-lEfruQ-t500x500.jpg",
       },
     ],
@@ -40,7 +40,7 @@ export default class App extends Component {
       <View style={styles.container}>
         <FlatList
           data={this.state.urls}
-          renderItem={({ item }) => <Imagem imagem={item.livro} />}
+          renderItem={({ item }) => <Imagem imagem={item.imagem} />}
         />
       </View>
     );
@@ -49,6 +49,7 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 50,
     flex: 1,
     backgroundColor: "beige",
     flexDirection: "column",
